206104602382329b66dba36d48b13c57d0ef5225
Sachin soni <107230805+sachin0612@users.noreply.github.com>
2024-04-02 16:37:50 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,2 +1,4 @@
 print(2+2)
 awk -F '|' 'NR > 2 && NF > 1 {gsub(/^[ \t]+|[ \t]+$/, "", $2); gsub(/^[ \t]+|[ \t]+$/, "", $3); print "Library:", $2, "Vulnerability ID:", $3}' trivy_output.txt
+grep -Eo '[[:alnum:]-]+[[:space:]]*\|[[:space:]]*[[:alnum:].-]+[[:space:]]*\|' trivy_output.txt
+
